ca4d293e7f633a6de5367173978e0e682b3331fc
PieTw3lve <ericlim000@outlook.com>
2024-03-08 16:18:36 -0500

    Cache Dates

--- src/main/java/com/github/pietw3lve/datecalculator/DateCalculatorPlugin.java
+++ src/main/java/com/github/pietw3lve/datecalculator/DateCalculatorPlugin.java
@@ -4,10 +4,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoUnit;
 
 public class DateCalculatorPlugin extends JavaPlugin
 {
+
+    private LocalDate today;
+    private long lastUpdated;
 
     public void onEnable()
     {
@@ -21,13 +26,22 @@ public class DateCalculatorPlugin extends JavaPlugin
     }
 
     public String getDaysBetweenToday(String targetDate) {
+        long now = System.currentTimeMillis();
+        if (today == null || now - lastUpdated >= 86400000) {
+            today = LocalDate.now();
+            lastUpdated = now;
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         try {
-            LocalDate today = LocalDate.now();
-            LocalDate target = LocalDate.parse(targetDate);
-            long days = ChronoUnit.DAYS.between(target, today);
-            return String.valueOf(days);
-        } catch (Exception e) {
+            LocalDate target = LocalDate.parse(targetDate, formatter);
+            if (target != null) {
+                long days = ChronoUnit.DAYS.between(target, today);
+                return String.valueOf(days);
+            }
+        } catch (DateTimeParseException e) {
             return "Invalid date format!";
         }
+        return "Invalid date format!";
     }
 }
