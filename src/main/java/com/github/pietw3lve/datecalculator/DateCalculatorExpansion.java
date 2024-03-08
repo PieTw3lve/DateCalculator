97ae99f084f51304c1db375e9fa28c4627d2eea5
PieTw3lve <ericlim000@outlook.com>
2024-03-08 16:26:18 -0500

    Update Version

--- src/main/java/com/github/pietw3lve/datecalculator/DateCalculatorExpansion.java
+++ src/main/java/com/github/pietw3lve/datecalculator/DateCalculatorExpansion.java
@@ -24,7 +24,7 @@ public class DateCalculatorExpansion extends PlaceholderExpansion {
 
     @Override
     public String getVersion() {
-        return "1.0.0";
+        return "1.0.1";
     }
 
     @Override
